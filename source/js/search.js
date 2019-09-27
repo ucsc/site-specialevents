6a41f4f290fd201c2ba4aebb5be88d0396880230
LuckyLuke007 <trung@scgstudio.com>
2019-09-27 14:16:59 -0700

    trying to make search to work

--- source/js/search.js
+++ source/js/search.js
@@ -9,7 +9,7 @@ jQuery(function() {
   });
 
   // Get the generated search_data.json file so lunr.js can search it locally.
-  window.data = $.getJSON('/search_data.json');
+  window.data = $.getJSON('/specialevents-site/search_data.json');
 
   // Wait for the data to load and add it to lunr
   window.data.then(function(loaded_data){
